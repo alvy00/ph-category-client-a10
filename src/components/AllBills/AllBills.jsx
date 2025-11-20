65918102d78cb98770694e123c9ff13548016e5d
Alvy <alvyahmed03@gmail.com>
2025-11-20 13:54:18 +0600

    Add new bill functionality added

--- src/components/AllBills/AllBills.jsx
+++ src/components/AllBills/AllBills.jsx
@@ -2,6 +2,7 @@ import { useRouteLoaderData } from "react-router";
 import BillCard from "../RecentBills/BillCard";
 import FilterDropdown from "./FilterDropdown";
 import { useState } from "react";
+import AddBill from "./AddBill";
 
 const AllBills = () => {
     const bills = useRouteLoaderData("Root");
@@ -12,9 +13,10 @@ const AllBills = () => {
             <div className="flex justify-between">
                 <span className="font-bold text-4xl">All Bills</span>
                 <div className="flex gap-2 justify-between items-center">
-                    <button className="btn btn-outline btn-success">
+                    {/* <button className="btn btn-outline btn-success">
                         Add Bill
-                    </button>
+                    </button> */}
+                    <AddBill />
                     <FilterDropdown
                         bills={bills}
                         setFilteredBills={setFilteredBills}
